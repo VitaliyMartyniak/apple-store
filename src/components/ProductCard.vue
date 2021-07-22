9185d24391d9fe7a40c025fa79743a0952b151fd
vitalii-martyniak <vitalii.martyniak@sombrainc.com>
2021-07-22 23:09:48 +0300

    Added product cards with description

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class='products__item'>
+    <div class='item__photo'>
+      <img :src="require(`@/assets/img/${productType}/${product.photo}.jpg`)" :alt='product.model'/>
+    </div>
+
+    <div class="item__description" v-if="productType === 'iphone'">
+      <p class='item__title'><span v-if="product.condition === 'used'">used</span>
+        {{product.model}} {{product.memory}}GB
+        <span v-if="product.hasTwoSim === 'yes'">Dual SIM</span> ({{product.color}})</p>
+    </div>
+    <div class="item__description" v-if="productType === 'mac'">
+      <p class="item__title"> {{product.model}} {{product.diagonal}} {{product.memory}} ({{product.color}})
+        <span v-if="product.hasTouchBar === 'yes'">with Touch Bar</span>
+      </p>
+    </div>
+    <div class="item__description" v-if="productType === 'watch'">
+      <p class="item__title">Apple Watch Series {{product.generation}}
+        {{product.size}}mm {{product.color}}
+        <span v-if="product.model === 'Apple Watch Sport'">Aluminium Case</span>
+        <span v-if="product.model === 'Apple Watch Stainless'">Steel Case</span>
+        <span v-if="product.model === 'Apple Watch Edition'">Ceramic Case</span>
+        with {{product.strapColor}} {{product.strapSeries}}
+      </p>
+    </div>
+
+    <hr/>
+    <h3 class='item__price'>{{product.price}} $</h3>
+    <!--      <button class='btn' (click)='addItem(product)'>Add to Cart</button>-->
+  </div>
+</template>
+
+<script lang='ts'>
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    productType: String,
+    product: Object
+  }
+})
+export default class IphoneProductCard extends Vue {
+  productType!: string
+  product!: any
+}
+</script>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,16 +1,11 @@
 <template>
   <div class='products'>
-    <div class='products__item' v-for='product of products' :key='product.id'>
-      <div class='item__photo'>
-        <img :src='getProductPhoto(product)' :alt='product.model'/>
-      </div>
-<!--      <p class='item__title'><span *ngIf='product.condition === 'used''>used</span>-->
-<!--        {{product.model}} {{product.memory}}GB-->
-<!--        <span *ngIf='product.hasTwoSim === 'yes''>Dual SIM</span> ({{product.color}})</p>-->
-<!--      <hr/>-->
-<!--      <h3 class='item__price'>{{product.price | currency}}</h3>-->
-<!--      <button class='btn' (click)='addItem(product)'>Add to Cart</button>-->
-    </div>
+    <ProductCard
+      v-for='product of products'
+      :key='product.id'
+      :product-type="productType"
+      :product="product"
+    />
   </div>
 <!--  <div class='catalog'>-->
 <!--    <ng-container *ngIf='(products-->
@@ -42,8 +37,10 @@
 
 <script lang='ts'>
 import { Options, Vue } from 'vue-class-component'
+import ProductCard from '@/components/ProductCard.vue'
 
 @Options({
+  components: { ProductCard },
   props: {
     productType: String
   }
@@ -63,32 +60,31 @@ export default class Products extends Vue {
       id: '1'
     },
     {
-      model: 'MacBook Pro',
-      diagonal: '13 inches',
-      memory: '128GB (SSD)',
-      color: 'Silver',
-      hasTouchBar: 'no',
-      price: 1339,
-      photo: 'macbook-13-silver',
+      model: 'iPhone 11 Pro Max',
+      color: 'Space Gray',
+      memory: 256,
+      hasTwoSim: 'yes',
+      condition: 'used',
+      price: 1299,
+      photo: '11pro-space',
       countInCart: 1,
-      id: '60'
+      id: '2'
     },
     {
-      generation: 4,
-      model: 'Apple Watch Sport',
-      size: 40,
-      color: 'Gold',
-      strapSeries: 'Sport Band',
-      strapColor: 'Pink Sand',
-      price: 429,
-      photo: 'sport-40-gold',
+      model: 'iPhone 11 Pro Max',
+      color: 'Silver',
+      memory: 256,
+      hasTwoSim: 'no',
+      condition: 'used',
+      price: 1199,
+      photo: '11pro-silver',
       countInCart: 1,
-      id: '87'
+      id: '3'
     }
   ]
 
   getProductPhoto (product: any): string {
-    return '../../assets/img/' + this.productType + '/' + product.photo + '.jpg'
+    return '@/assets/img/' + this.productType + '/' + product.photo + '.jpg'
   }
 }
 </script>
